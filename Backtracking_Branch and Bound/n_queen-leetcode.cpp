9330d4778e9790826d3241594aebd500953dffd0
Vicky-Jha <53377467+Vicky-Jha@users.noreply.github.com>
2022-06-08 22:01:07 +0530

    shorter code

--- Backtracking_Branch and Bound/n_queen-leetcode.cpp
+++ Backtracking_Branch and Bound/n_queen-leetcode.cpp
@@ -6,14 +6,14 @@ bool valid(vector<string> pushme, int i, int j) {
             }
         }
         
-        // crossRight
+        // diagonalRight
         for(int crossRight = i - 1, tempJ = j + 1; crossRight >= 0 && tempJ < pushme[0].size(); crossRight--, tempJ++) {
             if(pushme[crossRight][tempJ] == 'Q') {
                 return false;
             }
         }
         
-        // crossLeft
+        // diagonalLeft
         for(int crossLeft = i - 1, tempJ = j - 1; crossLeft >= 0 && tempJ >= 0; --crossLeft, --tempJ) {
             if(pushme[crossLeft][tempJ] == 'Q') {
                 return false;
@@ -30,29 +30,23 @@ bool valid(vector<string> pushme, int i, int j) {
             return;
         }
         
-        for(int k = j; k < n; ++k) {
-            reuse[k] = 'Q';
-            pushme.push_back(reuse);
+        for(int k = 0; k < n; ++k) {
             if(valid(pushme, i, k)) {
+                reuse[k] = 'Q';
+                pushme.push_back(reuse);
                 reuse[k] = '.';
                 backtrack(i + 1, 0, n, sol, pushme, reuse);
+                pushme.pop_back();
             }
-            reuse[k] = '.';
-            pushme.pop_back();
         }
     }
     
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> sol;
+        vector<string> pushme;
+        string reuse(n, '.');
         
-        for(int i = 0; i < n; ++i) {
-            vector<string> pushme;
-            string reuse(n, '.');
-            reuse[i] = 'Q';
-            pushme.push_back(reuse);
-            reuse[i] = '.';
-            backtrack(1, 0, n, sol, pushme, reuse);
-        }
+        backtrack(0, 0, n, sol, pushme, reuse);
         
         return sol;
     }
